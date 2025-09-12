93ab0360815e0109c4ebad8cdcb1c2e5ddecb51c
hyeon <819jeonghyeon@gmail.com>
2025-09-12 15:05:01 +0900

    0912

---
+++ Algorithm/Algorithm/src/BOJ_02_조건문/BOJ_14681.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class BOJ_14681 {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
+		
+		if (x > 0) {
+			if (y > 0) {
+				System.out.print(1);
+			}
+		}
+		
+		if (x > 0) {
+			if (y < 0) {
+				System.out.print(4);
+			}
+		}
+		
+		if (x < 0) {
+			if (y > 0) {
+				System.out.print(2);
+			}
+		}
+		
+		if (x < 0) {
+			if (y < 0) {
+				System.out.print(3);
+			}
+		}
+	} // main
+} // class
+}
